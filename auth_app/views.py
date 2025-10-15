164907967fb9a7a66f12b6b0e15e37591a3d7933
Kushan Gayantha <kushangayantha001@gmail.com>
2025-10-15 10:44:42 +0530

    function to class based view

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render,redirect
-from django.http import HttpRequest
+from django.http import HttpRequest,HttpResponse
+from django.views import View
+
 
 
 def index(req:HttpRequest):
     return render(req,'index.html')
 
 # Create your views here.
-def sign_up(req:HttpRequest):
+class SignIn(View):
 
-    if req.user.is_authenticated:
-        return redirect('/')
+    def get(self,req:HttpRequest):
 
-    return render(req, 'auth_app/sign-up.html')
+        return render(req,'auth_app/sign-up.html')
+    
+    def post(self,req:HttpRequest):
+
+        resp=HttpResponse()
+        resp.status_code=201
+
+        return resp
