a3db23889793653495f3158e5e2b941c21ad4288
Kushan Gayantha <kushangayantha001@gmail.com>
2025-10-16 11:03:27 +0530

    att updated

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -16,4 +16,11 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('first_name','last_name','email','bio','dob')
 
+        widgets={
+            "first_name":forms.TextInput(attrs={
+                "class":"w-full px-3 py-2 text-sm leading-tight text-gray-700  border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+             })
+            
+        }
+
     
