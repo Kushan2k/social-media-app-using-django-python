dc3758321e259caf1b5f4349dbcd60f5c39a145c
ilabs <kushangayantha001@gmail.com>
2025-10-17 15:18:02 +0530

    updated

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('sign-up', views.SignUp.as_view(), name='sign_up'),
     path('sign-in', views.SignIn.as_view(), name='sign-in',),
     path('sign-out',auth_views.LogoutView.as_view(next_page='sign-in'),name='sign-out'),
-    path('verify-account',view=views.VerifyAccount.as_view(),name='verify-account')
+    path('verify-account',view=views.VerifyAccount.as_view(),name='verify-account'),
+
+    path("change-password",view=views.CustomPasswordChangeView.as_view(),name='change-password'),
+    path('password-reset',auth_views.PasswordResetView.as_view(
+        template_name='password-reset.html',),name='password-reset'),
 
 ]
